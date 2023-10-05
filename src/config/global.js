3c97c6bf345863fb379d4076abdbc686ba0ce656
Andrea Botello <andrea.botellor@misena.edu.co>
2023-10-05 09:30:06 -0500

    Accesibilidad y .zip

--- src/config/global.js
+++ src/config/global.js
@@ -99,7 +99,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF07_228133_DU.zip',
       },
       {
         icono: 'fas fa-download',
